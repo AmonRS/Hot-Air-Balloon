af31392afe553f0cef7fee198927cebb092e82aa
AmonRS <amonsthapit@live.com>
2018-07-04 15:49:47 -0700

    init

---
+++ Assets/score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class score : MonoBehaviour {
+
+	public Text scoretext;
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoretext.text = "Score: " + (player.position.y*50).ToString("0");
+	}
+}
