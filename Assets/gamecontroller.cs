869c2f4e34eafcecf35fb603f7290e88e31d67d7
AmonRS <amonsthapit@live.com>
2018-07-06 12:22:04 -0700

    scrolling background + falling pickups + collisions + other stuff...

---
+++ Assets/gamecontroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamecontroller : MonoBehaviour {
+
+	public GameObject pickup;
+	public Vector3 spawnValues;
+
+	void Start () {
+		StartCoroutine (SpawnWaves ());
+	}
+	
+	void Update () {
+		
+	}
+
+	IEnumerator SpawnWaves () {
+        while (true) {
+			yield return new WaitForSeconds (1f);   //wait time
+
+			Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+			Instantiate (pickup, spawnPosition, transform.rotation);
+        }
+    }
+}
---
+++ Assets/pickup/pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pickup : MonoBehaviour {
+
+	Slider fuel;
+	
+	void Start() {
+		fuel = GameObject.Find("Slider").GetComponent<Slider>();
+
+		Destroy(gameObject, 10);   //self destruct object in a while
+	}
+
+	void Update() {
+		float r = 30*Time.deltaTime;
+		transform.Rotate(r,r,r);
+		transform.position += Vector3.down * Time.deltaTime * 5f;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("---> " + other.gameObject.name + "---" + gameObject.name + "-------" + fuel.name);
+
+		if (other.gameObject.name=="ballooon"){   // destroy pickup if touched
+			Destroy(gameObject);
+			fuel.value=+30;
+		}
+    }
+
+}
